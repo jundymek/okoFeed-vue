6f87a6b6b64ee2cf7a192e331a9e7ebebb334051
jundymek <jundymek@gmail.com>
2020-11-30 23:40:56 +0100

    feat: Post component - working load on scroll

---
+++ src/components/Post.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post">
+    <h2>{{ post.title }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    post: Object
+  }
+};
+</script>
+<style scoped>
+.post {
+  background: #fff;
+  padding: 1.5em;
+}
+.post:not(:last-child) {
+  border-bottom: 1px solid #ddd;
+}
+.post h2 {
+  font-size: 1.3em;
+  padding-bottom: 0.25rem;
+}
+.post p {
+  color: #888;
+}
+</style>
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,34 +1,43 @@
 <template>
-  <ul class="posts">
-    <li v-for="post in posts" :key="post.title">
+  <div class="scrolling-component" ref="scrollComponent">
+    <post v-for="post in posts" :key="post.title" :post="post" />
+  </div>
+  <!-- <ul class="posts" ref="scrollComponent">
+    <li v-for="post in posts" :key="post.title" class="list-element">
       {{ post.title }}
     </li>
-  </ul>
-  <button @click="getPosts">Pobierz więcej</button>
+  </ul> -->
+  <div ref="scrollComponent">Dupa</div>
+  <!-- <button @click="getPosts">Pobierz więcej</button> -->
 </template>
 
-<script lang="ts">
-export interface SinglePostType {
-  title: string;
-  thumb: string;
-  date: string;
-  excerpt: string;
-  url: string;
-}
-import { defineComponent } from "vue";
-const Posts = defineComponent({
-  name: "posts",
+<script>
+// export interface SinglePostType {
+//   title: string;
+//   thumb: string;
+//   date: string;
+//   excerpt: string;
+//   url: string;
+// }
+import { ref, onMounted, onUnmounted } from "vue";
+import Post from "./Post.vue";
 
-  data() {
-    return {
-      posts: [] as SinglePostType[],
-      totalNumberOfRecords: 0
-    };
-  },
+const getNewPosts = async index => {
+  console.log(index);
+  const response = await fetch(
+    `http://localhost:3000/posts?_start=${index}&_limit=10`
+  );
+  // const numberOfRecords = response.headers.get("x-total-count");
+  const newPosts = await response.json();
+  console.log({ newPosts });
+  return newPosts;
+  // console.log(newPosts);
+  // console.log(this.totalNumberOfRecords);
+};
 
-  mounted() {
-    this.getPosts();
-  },
+export default {
+  components: { Post },
+  name: "Posts",
 
   methods: {
     async getPosts() {
@@ -43,17 +52,64 @@ const Posts = defineComponent({
       }
       const newPosts = await response.json();
       this.posts = [...this.posts, ...newPosts];
-      console.log(newPosts);
-      console.log(this.totalNumberOfRecords);
+      return newPosts;
+      // console.log(newPosts);
+      // console.log(this.totalNumberOfRecords);
     }
-  }
-});
+  },
 
-export default Posts;
+  setup() {
+    const posts = ref([]);
+    const scrollComponent = ref(null);
+
+    const loadMorePosts = async () => {
+      console.log("loadMorePosts");
+      const anotherPosts = await getNewPosts(posts.value.length);
+      posts.value.push(...anotherPosts);
+    };
+
+    onMounted(loadMorePosts);
+
+    const handleScroll = e => {
+      console.log(e);
+      const element = scrollComponent.value;
+      console.log(element);
+      if (
+        element &&
+        element.getBoundingClientRect().bottom < window.innerHeight
+      ) {
+        console.log("load");
+        loadMorePosts();
+        // console.log(posts.value);
+      }
+    };
+    onMounted(() => {
+      window.addEventListener("scroll", handleScroll);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("scroll", handleScroll);
+    });
+
+    return {
+      posts,
+      scrollComponent
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .posts {
   background-color: paleturquoise;
+}
+
+.list-element {
+  height: 130px;
+}
+.scrolling-component {
+  width: 40%;
+  min-width: 300px;
+  margin: 0 auto;
 }
 </style>
