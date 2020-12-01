ff7daea604916242ef72327f4e136e60a43aa6af
jundymek <jundymek@gmail.com>
2020-12-01 11:33:08 +0100

    style: Post

--- src/components/posts/Posts.vue
+++ src/components/posts/Posts.vue
@@ -25,14 +25,12 @@ export default {
 
     const handleFetchMore = async () => {
       state.isLoading = true;
-      setTimeout(async () => {
-        const anotherPosts = await fetchPosts(state.posts.length);
-        state.posts.push(...anotherPosts);
-        if (anotherPosts.length < 10) {
-          state.isFull = true;
-        }
-        state.isLoading = false;
-      }, 3000);
+      const anotherPosts = await fetchPosts(state.posts.length);
+      state.posts.push(...anotherPosts);
+      if (anotherPosts.length < 10) {
+        state.isFull = true;
+      }
+      state.isLoading = false;
     };
 
     onMounted(handleFetchMore);
@@ -41,7 +39,7 @@ export default {
       const element = scrollComponent.value;
       if (
         element &&
-        element.getBoundingClientRect().bottom < window.innerHeight &&
+        element.getBoundingClientRect().bottom < window.innerHeight + 20 &&
         !state.isFull
       ) {
         handleFetchMore();
@@ -71,8 +69,8 @@ export default {
   @media (min-width: 640px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
-  @media (min-width: 640px) {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
   }
 }
 </style>
--- src/components/posts/post/Post.vue
+++ src/components/posts/post/Post.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="post">
-    <h2>{{ post.title }}</h2>
-  </div>
+  <section class="post">
+    <a v-bind:href="post.url" class="post__link">
+      <h3 class="post__title">{{ post.title }}</h3>
+      <img
+        v-bind:src="post.thumb"
+        alt=""
+        width="440"
+        height="280"
+        loading="lazy"
+        class="post__image"
+      />
+      <p class="post__description">{{ post.excerpt }}</p>
+      <span class="post__date">{{ post.date }}</span>
+    </a>
+  </section>
 </template>
 
 <script>
@@ -11,19 +23,41 @@ export default {
   }
 };
 </script>
-<style scoped>
+<style lang="scss" scoped>
 .post {
-  background: #fff;
-  padding: 1.5em;
-}
-.post:not(:last-child) {
-  border-bottom: 1px solid #ddd;
-}
-.post h2 {
-  font-size: 1.3em;
-  padding-bottom: 0.25rem;
-}
-.post p {
-  color: #888;
+  padding: 1rem;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  &:hover {
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+      0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  }
+
+  &__date {
+    float: right;
+    margin: 0.5rem;
+    color: #718096;
+    font-size: 0.875rem;
+  }
+
+  &__description {
+    padding-top: 0.5rem;
+    font-weight: 100;
+  }
+
+  &__image {
+    height: 12rem;
+    width: 100%;
+  }
+
+  &__link {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  &__title {
+    height: 6rem;
+    font-weight: bold;
+  }
 }
 </style>
