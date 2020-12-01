e82e6d2abd9d9a54ec03fe4e4727e32eb867526c
jundymek <jundymek@gmail.com>
2020-12-02 00:42:52 +0100

    feat: loading spinner

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <Header />
-  <Posts :posts="posts" @get-new-posts="getPosts" />
+  <Posts />
 </template>
 
 <script lang="ts">
--- src/components/posts/Posts.vue
+++ src/components/posts/Posts.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="posts-container container" ref="scrollComponent">
+  <div
+    class="posts-container container"
+    :class="{ inactive: state.isLoading }"
+    ref="scrollComponent"
+  >
     <post v-for="post in state.posts" :key="post.title" :post="post" />
   </div>
-  <p v-if="state.isLoading">Loading...</p>
+  <div class="spinner">
+    <p v-if="state.isLoading" class="loader"></p>
+  </div>
   <div v-if="state.isFull">KONIEC</div>
 </template>
 
@@ -35,14 +41,16 @@ export default {
 
     const handleFetchMore = async () => {
       state.isLoading = true;
-      const anotherPosts: SinglePostType[] = await fetchPosts(
-        state.posts.length
-      );
-      state.posts = [...state.posts, ...anotherPosts];
-      if (anotherPosts.length < 10) {
-        state.isFull = true;
-      }
-      state.isLoading = false;
+      setTimeout(async () => {
+        const anotherPosts: SinglePostType[] = await fetchPosts(
+          state.posts.length
+        );
+        state.posts = [...state.posts, ...anotherPosts];
+        if (anotherPosts.length < 10) {
+          state.isFull = true;
+        }
+        state.isLoading = false;
+      }, 3000);
     };
 
     onMounted(handleFetchMore);
@@ -52,7 +60,8 @@ export default {
       if (
         element &&
         element.getBoundingClientRect().bottom < window.innerHeight + 20 &&
-        !state.isFull
+        !state.isFull &&
+        !state.isLoading
       ) {
         handleFetchMore();
       }
@@ -78,11 +87,52 @@ export default {
   display: grid;
   gap: 1.5rem;
   padding-top: 6rem;
+  position: relative;
+  width: 100%;
   @media (min-width: 640px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
   @media (min-width: 1024px) {
     grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+}
+
+.spinner {
+  position: fixed;
+  z-index: 100;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.inactive {
+  opacity: 0.2;
+  pointer-events: none;
+}
+
+.loader,
+.loader:after {
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+}
+.loader {
+  /* font-size: 0.2rem; */
+  text-indent: -9999rem;
+  border-top: 0.4rem solid #fc8181;
+  border-right: 0.4rem solid #fc8181;
+  border-bottom: 0.4rem solid #fc8181;
+  border-left: 0.4rem solid #181717;
+  transform: translateZ(0);
+  animation: load8 1.1s infinite linear;
+}
+
+@keyframes load8 {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
